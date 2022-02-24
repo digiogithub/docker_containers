7d6b50206448242b23f972c0b8b147174156e3cb
Jose Francisco Rives Lirola <jose@digio.es>
2022-02-24 09:25:21 +0100

    feat(meteor): Add meteor development container

---
+++ README.md
@@ -0,0 +1,4 @@
+# Docker containers for Lando and web development
+
+In DIGIO we are web developers, we use Lando for local development, that's a abstraction over Docker and Docker-Compose more developer friendly.
+
--- meteordev/rebuild.sh
+++ meteordev/rebuild.sh
@@ -2,5 +2,5 @@ if [ -z "$VERSION" ]
 then
       echo "\$VERSION is not defined, please set version variable"
 else
-      docker build -t josedigio/meteor-dev:$VERSION .
+      DOCKER_BUILDKIT=1 docker build -t josedigio/meteor-dev:$VERSION .
 fi
